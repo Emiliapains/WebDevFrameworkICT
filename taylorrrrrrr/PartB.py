762eb1a29809f842813baf96d6c632ca405aa59c
EmiliaPainsMASTER <63119131+Emiliapains@users.noreply.github.com>
2026-03-23 15:53:59 +0000

    partB

--- taylorrrrrrr/PartB.py
+++ taylorrrrrrr/PartB.py
@@ -33,6 +33,43 @@ class MyTestCase(unittest.TestCase):
         song2 = Song("Du Hast", "Rammstein", 1998)
         self.assertNotEqual(song1, song2)
 
+    #Method tests
+    def test_artist_add_song(self):
+        self.artist.add_song(self.song)
+        self.assertIn(self.song, self.artist.list_of_songs)
+    def test_artist_add_album(self):
+        self.artist.add_album(self.album)
+        self.assertIn(self.album, self.artist.list_of_albums)
+    def test_album_add_song(self):
+        self.album.add_song("Liebe Ist Fur Alle Da", 2009)
+        self.assertEqual(len(self.album.listOfSongs), 1)
+    def test_playlist_add_song(self):
+        self.playlist.add_song(self.song)
+        self.assertIn(self.song, self.playlist.listOfSongs)
+    def test_sort_playlist_asc(self):
+        self.playlist.add_song(Song("Liebe Ist Fur Alle Da", "Rammstein", 2009))
+        self.playlist.add_song(Song("Du Hast", "Rammstein", 1998))
+        self.playlist.sort_playlist("ASC")
+        self.assertEqual(self.playlist.listOfSongs[0].songTitle, "Du Hast")
+    def test_sort_playlist_desc(self):
+        self.playlist.add_song(Song("Liebe Ist Fur Alle Da", "Rammstein", 2009))
+        self.playlist.add_song(Song("Du Hast", "Rammstein", 1998))
+        self.playlist.sort_playlist("DESC")
+        self.assertEqual(self.playlist.listOfSongs[0].songTitle, "Liebe Ist Fur Alle Da")
+    def test_shuffle_playlist(self):
+        self.playlist.add_song(Song("A", "Artist", 2020))
+        self.playlist.add_song(Song("B", "Artist", 2020))
+        self.playlist.add_song(Song("C", "Artist", 2020))
+        original_order = [
+            song.songTitle
+            for song in self.playlist.listOfSongs
+        ]
+        self.playlist.shuffle_playlist()
+        shuffled_order = [
+            song.songTitle
+            for song in self.playlist.listOfSongs
+        ]
+        self.assertNotEqual(original_order, shuffled_order)
 
 if __name__ == '__main__':
     unittest.main()
